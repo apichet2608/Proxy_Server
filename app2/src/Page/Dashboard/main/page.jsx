de3a93b9b17b6a4a95b19980e916ef61122a051b
Apichet Juntodum <Apichet.jtd@outlook.com>
2024-06-11 11:53:08 +0700

    Update nginx configuration to forward traffic from port 8080 to port 80 Update Vite base paths for app1 and app2 to include the respective subdirectories

--- app2/src/Page/Dashboard/main/page.jsx
+++ app2/src/Page/Dashboard/main/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import PicHsow from "../../../../public/Pic/Fuji.png";
 function Dashboard1() {
-  return <>Dashboard I</>;
+  return (
+    <>
+      Dashboard I
+      <img src={PicHsow} alt="Fuji" />
+    </>
+  );
 }
 
 export default Dashboard1;
